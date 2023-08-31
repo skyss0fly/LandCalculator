8195c4800e0269c0092ec67a167bb736a2b96776
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-31 19:32:24 +1000

    Update Main.php

--- src/skyss0fly/LandCalculator/Main.php
+++ src/skyss0fly/LandCalculator/Main.php
@@ -19,3 +19,38 @@
 */
 
 use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+
+
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $this->getLogger()->warning("Please use this command in-game");
+            return false;
+        }
+
+        $x = $sender->getPosition()->getX();
+        $y = $sender->getPosition()->getY();
+        $z = $sender->getPosition()->getZ();
+        $coords = $x . $y . $z;
+
+        switch ($command->getName()) {
+            case "pos1":
+                  
+                  return $coords;
+
+        }
+             case "pos2":
+
+
+        }
+
+  }
+
+          public function calculate(): int {
+
+          }
+
+
+}
